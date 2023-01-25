b136f6944db600d1abea68156a518e66044ba67e
kuminhdey <kuminhdey@gmail.com>
2023-01-25 14:46:52 +0700

    Update robot.js

--- robot.js
+++ robot.js
@@ -1,9 +1,3 @@
 var body = $response.body;
-var obj = JSON.parse(body);
-
-obj['result'] = obj['result'].replace("black_list_", "");;
-body = JSON.stringify(obj);
-
-console.log(body);
-
+body = body.replace("black_list_", "");
 $done(body);
