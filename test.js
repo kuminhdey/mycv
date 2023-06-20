1f6ef2c8de0b1663ce77caf4dc18c42f372de1ca
kuminhdey <kuminhdey@gmail.com>
2023-06-20 14:51:54 +0700

    Create test.js

---
+++ test.js
@@ -0,0 +1,9 @@
+function FindProxyForURL(url, host) {
+  // Cấu hình các quy tắc proxy dựa trên URL và host
+  if (shExpMatch(url, "*10.*")) {
+    return "DIRECT";
+  }
+  
+  // Quy tắc mặc định: không sử dụng proxy
+  return "PROXY 10.133.93.63:8080"
+}
